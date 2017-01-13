2ee7f9d7aad3bd5e6d96c63f47cac86cfd71678a
bosunolanrewaju <bosunolanrewaju@gmail.com>
2017-01-13 21:22:31 +0100

    Add requirejs config and payment handling file

--- Payments/view/frontend/web/js/view/payment/method-renderer/ravepayment-method.js
+++ Payments/view/frontend/web/js/view/payment/method-renderer/ravepayment-method.js
@@ -1,16 +1,63 @@
 define(
-    [
-        'Magento_Checkout/js/view/payment/default'
-    ],
-    function (Component) {
-        'use strict';
-        return Component.extend({
-            defaults: {
-                template: 'Rave_Payments/payment/ravepayment'
-            },
-            getMailingAddress: function () {
-                return window.checkoutConfig.payment.checkmo.mailingAddress;
-            },
+  [
+    'Magento_Checkout/js/view/payment/default'
+  ],
+  function(Component) {
+    'use strict';
+    return Component.extend({
+      defaults: {
+        template: 'Rave_Payments/payment/ravepayment'
+      },
+      getCustomerEmail: function() {
+        return quote.guestEmail || window.checkoutConfig.customerData.email;
+      },
+
+      getQuoteAmount: function() {
+        return window.checkoutConfig.quoteData.base_grand_total;
+      },
+
+      getQuoteCurrency: function() {
+        return window.checkoutConfig.quoteData.base_grand_total;
+      },
+
+      quoteRef: function() {
+        return 'MAGE_' + quote.getQuoteId();
+      },
+
+      /** Returns send check to info */
+      buildConfig: function() {
+        var option = window.checkoutConfig.payment.ravePayments;
+        var self = this;
+        return {
+          amount: this.getQuoteAmount(),
+          country: option.country,
+          currency: option.currency,
+          custom_description: option.modal_desc,
+          custom_logo: option.logo,
+          custom_title: option.modal_title,
+          customer_email: this.getCustomerEmail(),
+          PBFPubKey: option.pb_key,
+          txref: this.quoteRef(),
+          onclose: function() {
+            self.setErrorMessage("");
+            // redirectTo( redirectUrl );
+          },
+          callback: function(res) {
+            // sendPaymentRequestResponse( res, form );
+          }
+        };
+      },
+
+      /** Place Order action */
+      makePayment: function() {
+        getpaidSetup(this.buildConfig());
+      },
+
+      setErrorMessage: function(message) {
+        this.messageContainer.addErrorMessage({
+          message: 'Error ni oooooo'
         });
-    }
+      }
+    });
+  }
 );
