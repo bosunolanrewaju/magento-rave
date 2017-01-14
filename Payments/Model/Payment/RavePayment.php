5bf9f34e305ece909e25e03c79b8800db9f9ed6c
bosunolanrewaju <bosunolanrewaju@gmail.com>
2017-01-14 12:31:58 +0100

    Set isOffline to false

--- Payments/Model/Payment/RavePayment.php
+++ Payments/Model/Payment/RavePayment.php
@@ -2,19 +2,19 @@
 /**
  * Rave Payment gateway for accepting credit card, debit card and bank account payment on you store
  * Copyright (C) 2016  2017
- * 
+ *
  * This file is part of Rave/Payments.
- * 
+ *
  * Rave/Payments is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,7 +25,7 @@ class RavePayment extends \Magento\Payment\Model\Method\AbstractMethod
 {
 
     protected $_code = "ravepayment";
-    protected $_isOffline = true;
+    protected $_isOffline = false;
 
     public function isAvailable(
         \Magento\Quote\Api\Data\CartInterface $quote = null
